9c2994bcd6da44ca9a9e34c1b5e16a082e7a9d9c
Yura-Po <popatenkoyura2020@gmail.com>
2024-01-13 20:59:09 +0300

    I have included the css file |reset.css| and also renamed the file |block-header.php| ^_^

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/reset.css" rel="stylesheet">
     <title>Сайт просто окей...</title>
 </head>
 <body>
-    Hello World
+    <?php 
+    include("modules/block-header.php");
+    ?>
+    Hello world
 </body>
 </html>
