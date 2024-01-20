976551f4cd28ff9902e6dfa50d42d79fc80fa0d0
PC-Yura-Po <popatenkoyura2020@gmail.com>
2024-01-20 20:37:57 +0200

    Creating a layout for the site |^_^|

--- index.php
+++ index.php
@@ -4,12 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/reset.css" rel="stylesheet">
-    <title>Сайт просто окей...</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Інтернет-застосунок для оренди техніки!</title>
 </head>
 <body>
+    
+    <div class="block-body">
+
+    <div class="block-header">
     <?php 
     include("modules/block-header.php");
     ?>
-    Hello world
+    </div>
+
+<div class="block-right"></div>
+
+<div class="block-content"></div>
+    
+
+<?php
+include("modules/block-footer.php");
+?>
+    </div>
 </body>
 </html>
