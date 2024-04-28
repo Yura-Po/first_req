fcd75602969ddfef5d2c1e28c4cca1fa31eae842
YuraPo <popatenkoyura2020@gmail.com>
2024-04-28 11:32:06 +0300

    Created a burger menu ^_^

--- modules/block-header.php
+++ modules/block-header.php
@@ -1,10 +1,29 @@
-<div class="header-top-block">
-<ul class="header-top-menu">
-    <li>Ваш населений пункт - <span>Бердичів</span></li>
-    <li><a href="#">Про нас</a></li>
-    <li><a href="#">Контакти</a></li>
-    <li><a href="#">Розробники</a></li> 
-</ul>
-
-<p class="reg-auth-title"><a href="" class="top-auth">Вхід</a><a href="" class="top-auth-reg">Реєстрація</a></p>
-</div>
+<header class="header">
+    <div class="header__container">
+        <a href="#" class="header__logo"></a>
+        <div class="header__menu menu">
+            <div class="menu__icon">
+                <span></span>
+            </div>
+            <nav class="menu__body">
+                <ul class="menu__list">
+                    <li>
+                        <a href="" class="menu__link">Послуги</a>
+                        <span class="menu__arrow"></span>
+                        <ul class="menu__sub-list">
+                            <li>
+                            <a href="" class="menu__sub-link">Тур</a>
+                            </li>
+                            <li>
+                            <a href="" class="menu__sub-link">Залишити відгук</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="" class="menu__link">Про нас</a></li>
+                    <li><a href="" class="menu__link">Контакти</a></li>
+                    <li><a href="" class="menu__link">Вхід</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
